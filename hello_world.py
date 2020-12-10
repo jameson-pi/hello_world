8d3c5371f4cec41931fdc7b439b6e6c8c034e3b5
jameson-pi <75795824+jameson-pi@users.noreply.github.com>
2020-12-10 12:52:05 -0600

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+aliens = 0
+passward = "ALIENS"
+while aliens < 7200000 {
+  print()
+  print("WELCOME TO THE GLOBAL DEVENCE PLATFOLM")
+  print()
+  guiss = input("passward")
+  if guiss!=passward{
+    aliens ** 2
+    print("Incorrect passward.")
+    print("please try again")
+    print("HINT: what are attaching us")
+    guiss = input("try again")
+  else
+    print("thank you, all is saved")
+if aliens > 7200000 
+  print("no!!! all is lost")
+  brack
